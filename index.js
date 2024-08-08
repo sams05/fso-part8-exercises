7dda0a3ca27f71ec070109d05c4a93929b691382
Samuel Shen <sshen05@terpmail.umd.edu>
2024-08-08 12:10:26 -0400

    Finish ex8.2

--- index.js
+++ index.js
@@ -86,16 +86,25 @@ const books = [
 ]
 
 const typeDefs = `
+  type Book {
+    title: String!
+    author: String!
+    published: Int!
+    genres: [String!]!
+  }
+
   type Query {
     bookCount: Int
     authorCount: Int
+    allBooks: [Book!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
