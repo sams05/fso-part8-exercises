f8f47ad06fb5abf39b9c0d2adb4ea65f65c1f626
Samuel Shen <sshen05@terpmail.umd.edu>
2024-10-05 19:28:58 -0400

    Finish ex8.18

--- library-frontend/src/App.jsx
+++ library-frontend/src/App.jsx
@@ -1,9 +1,21 @@
-import { Routes, Route, Link } from 'react-router-dom'
+import { useState } from 'react'
+import { Routes, Route, Link, Navigate } from 'react-router-dom'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import Login from './components/Login'
+import { useApolloClient } from '@apollo/client'
 
 const App = () => {
+  const [token, setToken] = useState(null)
+  const client = useApolloClient()
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
   return (
     <div>
       <div>
@@ -13,15 +25,26 @@ const App = () => {
         <button>
           <Link to="/books">books</Link>
         </button>
-        <button>
-          <Link to="/new-book">add book</Link>
-        </button>
+        {token && (
+          <>
+            <button>
+              <Link to="/new-book">add book</Link>
+            </button>
+            <button onClick={logout}>logout</button>
+          </>
+        )}
+        {!token && (
+          <button>
+            <Link to="/login">login</Link>
+          </button>
+        )}
       </div>
 
       <Routes>
         <Route path="/" element={<Authors />} />
         <Route path="/books" element={<Books />} />
-        <Route path="/new-book" element={<NewBook />} />
+        <Route path="/new-book" element={token ? <NewBook /> : <Navigate replace to="/login" />} />
+        <Route path="/login" element={<Login setToken={setToken} />} />
       </Routes>
     </div>
   )
--- library-frontend/src/queries.js
+++ library-frontend/src/queries.js
@@ -24,22 +24,31 @@ export const ALL_BOOKS = gql`
 `
 
 export const ADD_BOOK = gql`
-  mutation AddBook($title: String!, $author: String!, $published: Int!, $genres: [String]) {
+  mutation AddBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
     addBook(title: $title, author: $author, published: $published, genres: $genres) {
       title
-      author
+      author {
+        name
+      }
       published
     }
   }
 `
 
 export const SET_BIRTHYEAR = gql`
-  mutation SetBithyear($name: String!, $born: Int) {
+  mutation SetBithyear($name: String!, $born: Int!) {
     editAuthor(name: $name, setBornTo: $born) {
       id
       name
       born
       bookCount
+    }
+  }
+`
+export const LOGIN = gql`
+  mutation Login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      value
     }
   }
 `
